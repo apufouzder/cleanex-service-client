7d45c7eb4f0b61d572b678411b1661b4a6a7a883
apufouzder <apufouzdar@gmail.com>
2021-04-18 21:05:31 +0600

    All File Upload And Done

--- src/components/AdminMenu/AllBookingListTable/AllBookingListTable.js
+++ src/components/AdminMenu/AllBookingListTable/AllBookingListTable.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 
-const AllBookingListTable = ({ booking, handleStatusChange }) => {
+const AllBookingListTable = ({ booking }) => {
     const [pending, setPending] = useState(booking.status.toLowerCase() === 'pending')
 
     const handlerStatus = (e) => {
